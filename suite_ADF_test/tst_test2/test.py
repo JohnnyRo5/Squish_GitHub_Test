293fd21c2451ab484d42cd58eacd2bc7dc411fec
JohnnyRo5 <168530310+JohnnyRo5@users.noreply.github.com>
2024-05-17 18:33:24 +0300

    Add files via upload

---
+++ suite_ADF_test/tst_test2/test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import names
+
+
+def main():
+    startApplication("adf-dei")
+    nativeType("<Alt+F4>")
+    test.compare(waitForObjectExists(names.launcherQmlView_Do_not_display_this_warning_anymore_Text).visible, True)
+    mouseClick(waitForObject(names.launcherQmlView_QUIT_Text))
