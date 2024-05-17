099b5894ff2e9cb1624dd57f3965e4783bbd3c47
JohnnyRo5 <168530310+JohnnyRo5@users.noreply.github.com>
2024-05-17 19:19:09 +0300

    Update test.py

--- suite_ADF_test/tst_test2/test.py
+++ suite_ADF_test/tst_test2/test.py
@@ -8,3 +8,4 @@ def main():
     nativeType("<Alt+F4>")
     test.compare(waitForObjectExists(names.launcherQmlView_Do_not_display_this_warning_anymore_Text).visible, True)
     mouseClick(waitForObject(names.launcherQmlView_QUIT_Text))
+    snooze(7)
