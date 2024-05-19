bcb7d21221f6df9831a9e039e38601f2ef42167d
JohnnyRo5 <168530310+JohnnyRo5@users.noreply.github.com>
2024-05-19 13:17:54 +0300

    Delete suite_ADF_test directory

--- suite_ADF_test/tst_demo_bdd/test.py
+++
@@ -1,8 +0,0 @@
-source(findFile('scripts', 'python/bdd.py'))
-
-source(findFile('scripts', '/bdd/steps.py'))
-source(findFile('scripts', '/bdd/bdd_hooks.py'))
-
-def main():
-    testSettings.throwOnFailure = True
-    runFeatureFile('test.feature')
