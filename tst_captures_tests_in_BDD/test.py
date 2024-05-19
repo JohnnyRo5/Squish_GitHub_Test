fbf2a0fd5fd9dec93d76c453d38c3c6e05af2d36
JohnnyRo5 <168530310+JohnnyRo5@users.noreply.github.com>
2024-05-19 13:31:02 +0300

    Delete tst_captures_tests_in_BDD directory

--- tst_captures_tests_in_BDD/test.py
+++
@@ -1,8 +0,0 @@
-source(findFile('scripts', 'python/bdd.py'))
-
-setupHooks('../../shared/steps/bdd_hooks.py')
-collectStepDefinitions('../../shared/steps')
-
-def main():
-    testSettings.throwOnFailure = True
-    runFeatureFile('test.feature')
